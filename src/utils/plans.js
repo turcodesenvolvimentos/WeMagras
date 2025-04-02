71a1fbf6858a998ccc96dbf2b5509abe9b3c5bd5
Vuinishiusu <ewerton.viniciusturco@gmail.com>
2025-04-01 21:47:14 -0300

    feat: icon check, gabiImage and [plans]

---
+++ src/utils/plans.js
@@ -0,0 +1,29 @@
+export default [
+  {
+    name: "Plano Básico",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu amet risus et ipsum.",
+    items: ["Acesso as aulas gravadas na plataforma HUBLA"],
+  },
+  {
+    name: "Mentoria Individual",
+    description:
+      "Ela vai te orientar e mostrar o passo a passo específico para você, para a sua realidade.",
+    items: [
+      "Acesso as aulas gravadas na plataforma HUBLA",
+      "Encontros ao vivo com a Gabi",
+      "Acesso ao grupo do Telegram para falar direto com a Gabi e a Nicole",
+      "Tenha sessões individuais com a Gabi",
+    ],
+  },
+  {
+    name: "Comunidade WeMagras",
+    description:
+      "A partir de 4 assinaturas conjuntas ganha 10% de desconto na matrícula",
+    items: [
+      "Acesso as aulas gravadas na plataforma HUBLA",
+      "Encontros ao vivo com a Gabi",
+      "Acesso ao grupo do Telegram para falar direto com a Gabi e a Nicole",
+    ],
+  },
+];
